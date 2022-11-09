f7b8ea26eacf4532709997689e8b1f5662dc1ec1
Edson Macamo <edsonmacamo@home.local>
2022-11-09 13:57:43 +0200

    primeiro committ

---
+++ olamundo.dart
@@ -0,0 +1,12 @@
+void main() {
+  String variavelNome = 'Edson';
+  int valorVariavel = 10;
+  bool everdadeiro = true;
+  List<String> listaDePalavras = ["Edson", "Macamo"];
+
+  print(variavelNome);
+  print(valorVariavel);
+  print(everdadeiro);
+
+  print('${listaDePalavras[0]} - ${listaDePalavras[1]}');
+}
